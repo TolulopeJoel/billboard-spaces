6012156fc8f76be96aae4db384dfb24bbfcaece4
Tolu Joel <toluisjoel@gmail.com>
2024-01-24 15:57:50 +0100

    fix: THIS SHOULD BE IT!

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-set DJANGO_SETTINGS_MODULE=billboard_spaces.settings
-
 python3 -m pip install -r requirements.txt
+
 python3 manage.py collectstatic --no-input
+
 python3 manage.py migrate
