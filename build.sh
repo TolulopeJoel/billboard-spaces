df3da8d614c282e98f8fa17f2cadb764004f8611
Tolu Joel <toluisjoel@gmail.com>
2024-01-24 15:28:00 +0100

    chore: Update `build.sh`

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 
-python -m pip install -r requirements.txt
-
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
