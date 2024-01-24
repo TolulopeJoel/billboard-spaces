14fa1c5572c4d7e53d0dc28ae3fc879199c3631e
Tolu Joel <toluisjoel@gmail.com>
2024-01-24 16:07:37 +0100

    chore: Update `build.sh`

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+python -m pip install -r requirements.txt
 
-python3 -m pip install -r requirements.txt
+ python manage.py collectstatic --no-input
+ python manage.py migrate
 
-python3 manage.py collectstatic --no-input
-
-python3 manage.py migrate
+ echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('theadmin@example.com', 'password'); print(User.objects.all())" | python manage.py shell
