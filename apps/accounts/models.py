524c07b28a44b638dc3c61655e3784d104bc3422
Tolu Joel <toluisjoel@gmail.com>
2024-02-07 23:55:04 +0100

    feat: Seperate user model from profile details - create new model (Profile)

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from utils.constants import NIGERIAN_STATES, USER_CHOICES
 
+from .choices import Country, State, UserField
 from .managers import CustomUserManager
 
 
@@ -16,24 +16,9 @@ class CustomUser(AbstractUser):
         editable=False,
         unique=True
     )
+
     username = None
-
     email = models.EmailField(_("email address"), max_length=100, unique=True)
-
-    # Update profile requirements
-    user_field = models.CharField(
-        max_length=20, choices=USER_CHOICES, blank=True, null=True
-    )
-    full_name = models.CharField(max_length=255, blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    state_of_residence = models.CharField(
-        max_length=255, choices=NIGERIAN_STATES, blank=True, null=True
-    )
-    display_name = models.CharField(max_length=255, blank=True, null=True)
-
-    # Password reset rqeuirements
-    password_reset_otp = models.CharField(max_length=6, null=True)
-    password_reset_otp_created_at = models.DateTimeField(null=True)
 
     objects = CustomUserManager()
 
@@ -42,3 +27,32 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        related_name='profile',
+        null=True,
+    )
+
+    # User profile creation requirements
+    user_field = models.CharField(
+        max_length=20,
+        choices=UserField.choices,
+        null=True
+    )
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    display_name = models.CharField(max_length=255, null=True)
+    state = models.CharField(max_length=20, choices=State.choices, null=True)
+    country = models.CharField(
+        max_length=10,
+        choices=Country.choices,
+        default=Country.NIGERIA
+    )
+    phone_number = models.CharField(unique=True, max_length=11, null=True)
+
+    # Password reset rqeuirements
+    password_reset_otp = models.CharField(max_length=6, null=True)
+    password_reset_otp_created_at = models.DateTimeField(null=True)
